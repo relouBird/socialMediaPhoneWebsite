cc5325d8d49c757c2e7f96a1ea60e5074f7b3983
RelouBird <ulrichkwamou@gmail.com>
2024-04-01 19:42:31 +0100

    update the height on phone of website

--- src/components/logSign/SubmitButton.svelte
+++ src/components/logSign/SubmitButton.svelte
@@ -1,14 +1,13 @@
 <script lang="ts">
-    import type { MouseEventHandler } from "svelte/elements";
-    export let name : string;
-    export let handleClick : MouseEventHandler<HTMLButtonElement>
+  import type { MouseEventHandler } from "svelte/elements";
+  export let name: string;
+  export let handleClick: MouseEventHandler<HTMLButtonElement>;
 </script>
 
-
 <div class="mt-6">
-    <button
+  <button
     on:click|preventDefault={handleClick}
-     class="w-full pt-1.5 pb-2.5 text-center text-[16px] text-white rounded-md rounded-tr-none bg-blackslow"
-     >{name}
-    </button>
+    class="w-full pt-1.5 pb-2.5 text-center text-[16px] text-white rounded-md rounded-tr-none bg-blackslow"
+    >{name}
+  </button>
 </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,11 +2,14 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+	<link rel="icon" href="/icon.png" />
+</svelte:head>
+
 <div class="app w-full h-full">
 
-	<main class="w-full h-full sm:h-screen flex justify-center items-center bg-gray-100">
-		<div class="w-[100%] sm:w-[600px] h-screen sm:h-[90vh] sm:-translate-x-4 bg-white">
-			<!-- <div class="w-full h-16 bg-blue-200"></div> -->
+	<main class="w-full h-screen sm:h-screen flex justify-center sm:items-center bg-gray-100">
+		<div class="w-[100%] sm:w-[650px] h-full sm:overflow-auto sm:h-[90vh] sm:-translate-x-4 bg-white">
 			<slot />
 		</div>
 	</main>
