dc9220b9e2b13b5b35917b2e40537adcfb4bda54
RelouBird <ulrichkwamou@gmail.com>
2024-04-01 01:19:45 +0100

    initialize the global project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+
+<section>
+
+</section>
