b0201c8e28d0c6d38dfe66bb9c55e138e4adb6c2
RelouBird <ulrichkwamou@gmail.com>
2024-04-07 17:23:17 +0100

    Finishing the design of Profil

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,19 +4,6 @@
 
   let connect = isConnected();
 
-  import { writable } from "svelte/store";
-  export const imageURL = writable("");
-
-  const  handleFileInput: ChangeEventHandler<HTMLInputElement> = (event) => {
-    const inputElement = event.target as HTMLInputElement;
-    if (!inputElement.files) return;
-
-    const file = inputElement.files[0];
-    if (file) {
-      const url = URL.createObjectURL(file);
-      imageURL.set(url);
-    }
-  }
 </script>
 
 <svelte:head>
@@ -29,16 +16,9 @@
     class="w-full navbar-effect sm:navbar-effect sm:shadow bg-white overflow-hidden"
   >
     <div
-      class="w-full h-10 flex pt-8 px-2.5 sm:px-4 justify-between items-center"
+      class="w-full h-10 flex pt-8 text-lg font-medium text-black/70 px-2.5 sm:px-4 justify-between items-center"
     >
-      <script>
-      </script>
-
-      <input type="file" on:change={handleFileInput} />
-      {#if $imageURL}
-        <!-- svelte-ignore a11y-img-redundant-alt -->
-        <img src={$imageURL} alt="Selected Image" />
-      {/if}
+    Welcome on the Home view.
     </div>
   </section>
 {/if}
